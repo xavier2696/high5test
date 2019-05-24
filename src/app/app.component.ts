0635d0d0dbc1f001629835148df97c6ba5ff1a24
Xavier Munguia <xavier2696@gmail.com>
2019-05-24 17:50:20 +0800

    test project completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-    I = '1'
+    I = 1;
+    numbers_array = [0];
 
+    update_array(){
+    	this.numbers_array = [];
+    	for (var i = 0; i < Math.min(this.I, 10); i++){
+    		this.numbers_array.push(i);
+    	}
+    }
 }
